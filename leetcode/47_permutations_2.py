e2388f8eb827160510371dd2f9d72793342076ba
inkayat <inkayatolga@gmail.com>
2022-03-27 02:53:56 +0300

    problem 47 solved and tested

---
+++ leetcode/47_permutations_2.py
@@ -0,0 +1,8 @@
+from typing import List
+from itertools import permutations
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        return set(permutations(nums))
+
+
