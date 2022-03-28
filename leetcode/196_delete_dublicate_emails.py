a403af8a0f138e2488c34a468b2f0fc3b1ac5b0b
inkayat <inkayatolga@gmail.com>
2022-03-28 18:28:34 +0300

    problem 192 solved and tested in successs

---
+++ leetcode/196_delete_dublicate_emails.py
@@ -0,0 +1,3 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE n1 FROM Person n1, Person n2 WHERE n1.ID > n2.ID AND n1.email = n2.email
